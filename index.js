297377de716a1560344fb8f8483e439b7d54eb1e
andisiambuku <roselandambuku@gmail.com>
2022-08-22 23:36:27 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',() => {
+    console.log("The DOM has loaded");
+    let text = document.getElementById('text').textContent='This is really cool!'
+});
+
